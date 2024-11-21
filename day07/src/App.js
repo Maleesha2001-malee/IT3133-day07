c6e3db38aac71e7df5e4a484d1bc20f7b78aead0
Maleesha Rathnayake <139684247+Maleesha2001-malee@users.noreply.github.com>
2024-11-21 07:59:17 +0000

    add Childcom in app.js

--- day07/src/App.js
+++ day07/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import Childcom from './Childcom';
 
 function App() {
   const[childName,setChildname]=useState("Default");
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <h2>Hello React I'm Parent</h2>
       <p>Parent:My child said:{childName}</p>
-     
+      <Childcom setfun={receiveName}/>
     </div>
   );
 }
